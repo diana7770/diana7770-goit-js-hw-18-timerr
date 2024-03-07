15635188ab0fb2d78766be1b66445ca5a9aba48c
diana7770 <20diane09@gmail.com>
2024-03-07 03:31:26 +0200

    hw

---
+++ src/task_1/index_1.js
@@ -0,0 +1,23 @@
+const timer = document.querySelector(".timer");
+let time = 3600;
+
+const timeId = setInterval(timeOut, 1000);
+
+function timeOut() {
+  time--;
+
+  if (time === 0) {
+    clearInterval(timeId);
+    alert("Час вийшов!");
+  }
+
+  if (time <= 1800 && time % 60 === 0) {
+    alert("Залишилось менше половини часу!");
+  }
+
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time % 3600) / 60);
+  let seconds = time % 60;
+
+  timer.textContent = ` ${hours} годин   ${minutes} хвилин   ${seconds} секунд`;
+}
---
+++ src/task_2/index_2.js
@@ -0,0 +1,20 @@
+const timer = document.querySelector(".timer");
+let time = 30;
+
+const timeId = setInterval(timeOut, 100);
+
+function timeOut() {
+  time--;
+
+  if (time === 10) {
+    timer.classList.add("animate");
+  }
+
+  if (time === 0) {
+    clearInterval(timeId);
+    timer.style.backgroundImage =
+      "https://ukraflora.ua/image/cache/catalog/image/cache/catalog/bouquets/14.02.23/4837e948-e996-4377-a9c3-9524456d4997-1100x1100.webp";
+  }
+
+  timer.textContent = `${time} секунд`;
+}
